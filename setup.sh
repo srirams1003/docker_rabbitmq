0d3d4873b3ed2b3b5b38de180b5a70aba1b7fb45
srirams1003 <57574798+srirams1003@users.noreply.github.com>
2024-09-27 07:41:00 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@ cd /usr/src/app
 apt-get update
 apt-get install build-essential -y
 apt-get install librabbitmq-dev -y
-
+apt-get install vim -y
 
 # rm send receive
 g++ -o send send.cpp -lrabbitmq
